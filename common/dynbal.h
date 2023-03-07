45ec1c2b1648b93fba47aa9e56a9ec2d8e3a71ef
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2023-03-07 21:00:27 +0800

    Temporarily switch to Fixed tile size selection
    For check correctness of the new algorithm

--- common/dynbal.h
+++ common/dynbal.h
@@ -6,7 +6,7 @@
 #define Fixed 0
 #define DynBal 1
 #define Exhaustive 2
-#define RuntimeConfiguration DynBal
+#define RuntimeConfiguration Fixed
 
 const uint32_t NVM_RELATIVE_WRITE_COST = 1; // the ratio of NVM write cost and NVM read cost
 
