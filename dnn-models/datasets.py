02d4b00cc71076ab8dac1b32135996e7a62b5b95
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2023-04-23 17:53:00 +0800

    Rework on KWS cache to get rid of torchdatasets
    That package is incompatible with pytorch >= 1.12 as
    `torch.utils.data._typing` is moved. A change merged some time ago
    makes it work again [1], but no there is no release since then.
    
    [1] https://github.com/szymonmaszke/torchdatasets/pull/30

--- dnn-models/datasets.py
+++ dnn-models/datasets.py
@@ -9,7 +9,6 @@ import torchaudio
 import torchvision.datasets
 from torchvision.transforms import Compose, ToTensor, Resize
 from torch.utils.data import TensorDataset
-import torchdatasets as td
 
 from utils import (
     DataLayout,
@@ -30,58 +29,67 @@ def load_data_cifar10(train: bool, target_size: tuple[int, int]) -> ModelData:
         dataset = torchvision.datasets.CIFAR10(root=xdg_cache_home, train=train, download=True, transform=transforms)
     return ModelData(dataset=dataset, data_layout=DataLayout.NCHW)
 
-class SpeechCommands_MFCC(torchaudio.datasets.SPEECHCOMMANDS):
+def preprocess_kws_dataset(original_dataset):
     GOOGLE_SPEECH_SAMPLE_RATE = 16000
     # From https://github.com/ARM-software/ML-KWS-for-MCU/blob/master/Pretrained_models/labels.txt
     new_labels = '_silence_ _unknown_ yes no up down left right on off stop go'.split(' ')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.graph_def = None
+    import tensorflow as tf
 
-    def __getitem__(self, n):
-        import tensorflow as tf
+    with open(kws_dnn_model(), 'rb') as f:
+        graph_def = tf.compat.v1.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def)
 
-        if self.graph_def is None:
-            with open(kws_dnn_model(), 'rb') as f:
-                self.graph_def = tf.compat.v1.GraphDef()
-                self.graph_def.ParseFromString(f.read())
-                tf.import_graph_def(self.graph_def)
+    with tf.compat.v1.Session() as sess:
+        mfcc_tensor = sess.graph.get_tensor_by_name('Mfcc:0')
 
-        with tf.compat.v1.Session() as sess:
-            mfcc_tensor = sess.graph.get_tensor_by_name('Mfcc:0')
-
+        labels = []
+        mfccs = None  # The dimensions are unknown yet
+        dataset_size = len(original_dataset)
+        for n in range(dataset_size):
             # The first few _unknown_ samples are not recognized by Hello Edge's DNN model - use good ones instead
-            data = super().__getitem__(len(self) - 1 - n)
+            data = original_dataset[dataset_size - 1 - n]
             waveform, sample_rate, label, _, _ = data
-            assert sample_rate == self.GOOGLE_SPEECH_SAMPLE_RATE
+            assert sample_rate == GOOGLE_SPEECH_SAMPLE_RATE
             decoded_wav = np.squeeze(waveform)
             # Some files in speech_commands_v0.02 are less than 1 second. By comparing with corresponding files in
             # speech_commands_test_set_v0.02, zeros are padded at last apparently
-            decoded_wav = np.pad(decoded_wav, ((0, self.GOOGLE_SPEECH_SAMPLE_RATE - len(decoded_wav)),))
+            decoded_wav = np.pad(decoded_wav, ((0, GOOGLE_SPEECH_SAMPLE_RATE - len(decoded_wav)),))
             decoded_wav = np.expand_dims(decoded_wav, axis=-1)
 
             # See the logic at https://github.com/tensorflow/datasets/blob/v4.6.0/tensorflow_datasets/audio/speech_commands.py#L128-L140
-            if label in self.new_labels:
-                label = self.new_labels.index(label)
+            if label in new_labels:
+                label = new_labels.index(label)
             elif label in ('_silence_', '_background_noise_'):
-                label = self.new_labels.index('_silence_')
+                label = new_labels.index('_silence_')
             else:
-                label = self.new_labels.index('_unknown_')
+                label = new_labels.index('_unknown_')
+            labels.append(label)
 
             mfcc = sess.run(mfcc_tensor, {
                 'decoded_sample_data:0': decoded_wav,
-                'decoded_sample_data:1': self.GOOGLE_SPEECH_SAMPLE_RATE,
+                'decoded_sample_data:1': GOOGLE_SPEECH_SAMPLE_RATE,
             })
+            if mfccs is None:
+                mfccs_shape = list(mfcc.shape)
+                mfccs_shape[0] = dataset_size
+                mfccs = np.zeros(mfccs_shape)
+            mfccs[n, :, :] = mfcc
 
-        return mfcc[0], label
+    return mfccs, labels
 
 def load_data_google_speech(train: bool, target_size: tuple[int, int]) -> ModelData:
     xdg_cache_home = platformdirs.user_cache_path()
     with filelock.FileLock(xdg_cache_home / 'SpeechCommands.lock'):
-        dataset = SpeechCommands_MFCC(root=xdg_cache_home, download=True, subset='training' if train else 'testing')
-        # Using an on-disk cache as invoking TensorFlow for each data item is very slow
-        dataset = td.datasets.WrapDataset(dataset).cache(td.cachers.Pickle(xdg_cache_home / 'SpeechCommands_MFCC'))
+        kws_cache_filename = xdg_cache_home / 'SpeechCommands-cache-v1.pth'
+        if kws_cache_filename.exists():
+            mfccs, labels = torch.load(kws_cache_filename)
+        else:
+            original_dataset = torchaudio.datasets.SPEECHCOMMANDS(root=xdg_cache_home, download=True, subset='training' if train else 'testing')
+            mfccs, labels = preprocess_kws_dataset(original_dataset)
+            torch.save((mfccs, labels), kws_cache_filename)
+    dataset = TensorDataset(torch.from_numpy(mfccs.astype(np.float32)), torch.tensor(labels))
     assert dataset[0][0].shape == target_size
     return ModelData(dataset, data_layout=DataLayout.NEUTRAL)
 
